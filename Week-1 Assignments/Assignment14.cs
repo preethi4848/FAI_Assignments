0da6d2c4c440be355215ce4a9af93c09be8cf1ee
Preethi G <preethi4848@gmail.com>
2022-08-28 23:34:48 +0530

    Add files via upload

---
+++ Week-1 Assignments/Assignment14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Week1_Assignments
+{
+    interface IDataComponent
+    {
+        void Employee();
+    }
+    [Serializable]
+    class Employee
+    {
+        static int empNo = 1000;
+        public Employee()
+        {
+            EmpID = ++empNo;
+        }
+
+        public Employee(int id)
+        {
+            EmpID = id;
+        }
+        public int EmpID { get; private set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public int EmpSalary { get; set; }
+        public override string ToString()
+        {
+            return $"{EmpID}, {EmpName}, {EmpAddress}, {EmpSalary}";
+        }
+    }
+    class Assignment14
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
